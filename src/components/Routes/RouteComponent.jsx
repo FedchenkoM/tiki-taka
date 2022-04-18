b65865b495cd9451aa4e964be1ddc60f06f2423b
Федченко Михаил <fedchenko@softlab.ru>
2022-04-18 16:37:22 +0400

    feat(): Добавил роутинг по умолчанию

--- src/components/Routes/RouteComponent.jsx
+++ src/components/Routes/RouteComponent.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import MainPage from '../MainPage/MainPage';
 
 const RouteComponent = () => {
     return (
         <Routes>
-            <Route path='/mainPage' element={<h1>Main Page</h1>} />
+            <Route path='/mainPage' element={<MainPage />} />
             <Route path='/players' element={<h1>Players</h1>} />
+            <Route path="*" element={<Navigate to='/mainPage' />} />
         </Routes>
     );
 };
 
-export default RouteComponent;
+export default RouteComponent;  
